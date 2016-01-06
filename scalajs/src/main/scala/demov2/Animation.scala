bb96fbc46cffc14d9c64e72f6378da6449feb17c
Michael Hartl <mikehartl17@gmail.com>
2016-01-06 18:13:44 +0100

    use twodee instead of Geometry

--- scalajs/src/main/scala/demov2/Animation.scala
+++ scalajs/src/main/scala/demov2/Animation.scala
@@ -7,25 +7,15 @@ import org.scalajs.jquery.jQuery
 import scala.scalajs.js.annotation.JSExport
 import Math._
 
+import twodee.geometry._
 import twodee.shapes._
+import twodee.transform._
 
 object Animation {
 
-  import demov2.Canvas._
-  import demov2.Canvas.Geometry._
+  import Canvas.Anim
+  import Canvas.ColoredShape
 
-  def circle(center: CoordsPoint, radius: Double, colorF: ColorF) {
-    draw(BoundingBox(center + CoordsPoint(-radius, radius), 
-		     center + CoordsPoint(radius, -radius)),	 
-	 (p => {
-	   val relP = p + CoordsPoint(center.x * -1, center.y * -1)
-	   if(sqrt(relP.x*relP.x + relP.y*relP.y) <= radius) { 
-	     Some(colorF(p))
-	   } else {
-	     None
-	   }}))
-  }
-  
   def gradientVal(x: Double, kIntv: (Double, Double), vIntv: (Double, Double)) =
     kIntv match {
       case (minK, maxK) => vIntv match {
@@ -34,31 +24,36 @@ object Animation {
       }
     }
     
-  def sun() {
+  val sun = Anim(() => {
     val radius = 0.15
-    circle(CoordsPoint(0,0), radius,
-	   (p => {
-	     val d = sqrt(p.x*p.x+p.y*p.y)
-	     val r = 255
-	     val g = 240
-	     val b = round(gradientVal(d, (0,radius), (150,100)))
-	     s"rgb($r,$g,$b)"
-	   }))
-  }
+    val colorF = ((p: Vector) => {
+      val d = sqrt(p.x*p.x+p.y*p.y)
+      val r = 255
+      val g = 240
+      val b = round(gradientVal(d, (0,radius), (150,100)))
+      s"rgb($r,$g,$b)"
+    })
+    
+    ColoredShape(Circle(Scale(0.15)), colorF)
+  })
 
-  def earth() {
+  val earth = Anim(() => {
     val radius = 0.05
     val t = new java.util.Date().getTime/1000.0
-    val c = CoordsPoint(cos(t), sin(t))
-    circle(CoordsPoint(cos(t)*0.8, sin(t)*0.8), radius, (_ => "blue"))
+    val coords = Vector(cos(t)*0.8, sin(t)*0.8)
+    val shape  = Circle.unit scale radius translate coords
+    ColoredShape(shape, (_ => "blue"))
+  })
+
+  def animate() {
+    scala.scalajs.js.Dynamic.global.window.requestAnimationFrame(() => animate)
+    Canvas.redraw()
   }
 
   def drawSystem() {
-    scala.scalajs.js.Dynamic.global.window.requestAnimationFrame(() => drawSystem)
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    setSize()
-    background()
-    sun()
-    earth()
+    Canvas.init()
+    Canvas.add("sun", sun)
+    Canvas.add("earth", earth)
+    animate()
   }
 }
--- scalajs/src/main/scala/demov2/Canvas.scala
+++ scalajs/src/main/scala/demov2/Canvas.scala
@@ -1,6 +1,6 @@
 package demov2
 
-import scala.collection.immutable.HashMap
+import scala.collection.mutable.HashMap
 
 import org.scalajs.dom
 import org.scalajs.dom.html
@@ -9,60 +9,25 @@ import org.scalajs.jquery.jQuery
 import scala.scalajs.js.annotation.JSExport
 import Math._
 
-import twodee.shapes.Box
-import twodee.shapes.Shape
+import twodee.shapes._
+import twodee.geometry._
 
 object Canvas {
 
-  object Geometry {
-    
-    case class CoordsPoint(x: Double, y: Double) {
-      def +(q: CoordsPoint) = CoordsPoint(x + q.x, y + q.y)
-    }
-    
-    case class RasterPoint(x: Int, y: Int) {
-      def +(q: RasterPoint) = RasterPoint(x + q.x, y + q.y)
-    }
-    
-    trait ToRaster[T] {
-      def apply(t: T) : RasterPoint
-    }
+  type ColorF = Vector => String
 
-    trait ToCoords[T] {
-      def apply(t: T) : CoordsPoint
-    }
+  case class ColoredShape(shape: Shape, color: ColorF)
+  case class Anim(paintF: (() => ColoredShape))
 
-    implicit object CoordsToRaster extends ToRaster[CoordsPoint]{
-      def apply(p: CoordsPoint): RasterPoint = {
-	val halfw = canvas.width/2.0
-	val halfh = canvas.height/2.0
-	RasterPoint(round(halfw + halfw * p.x).toInt,
-		    round(halfh + halfh * p.y * -1).toInt)
-      }
-    }
-  
-    implicit object RasterToCoords extends ToCoords[RasterPoint]{
-      def apply(p: RasterPoint): CoordsPoint = {
-	val halfw = canvas.width/2.0
-	val halfh = canvas.height/2.0
-	CoordsPoint((p.x.toDouble - halfw) / canvas.width*2,
-		    ((p.y.toDouble - halfh) / canvas.height * -2))
-      }
-    }
-    
-    case class BoundingBox(upperLeft: CoordsPoint, lowerRight: CoordsPoint)
-  }
-
-  var objects: HashMap[String, (_ => Shape)] = HashMap.empty
-  
-  import Geometry._
-
-  type ColorF = CoordsPoint => String
+  private var objects: HashMap[String, (Option[Box], Anim)] = HashMap.empty
+  def add(id: String, anim: Anim) = objects += id -> (None, anim)
   
   type Context = dom.CanvasRenderingContext2D
 
   val canvas: Canvas = jQuery("#canvas")(0).asInstanceOf[Canvas]
   val ctx = canvas.getContext("2d").asInstanceOf[Context]
+  setSize()
+  val raster = Raster(canvas.width, Box.stdBox)
 
   def setSize() {
     val minW = 100
@@ -78,30 +43,55 @@ object Canvas {
     canvas.height = min(maxH, max(minH, min(screenW, screenH)))
   }
 
-  def background() {
+  private def rasterBox(box: Box) =
+    (raster.fromPoint(box.upperLeft), raster.fromPoint(box.lowerRight))
+
+  private def background(box: Box) {
+    val (ulr, lrr) = rasterBox(box)
     ctx.fillStyle = "black"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillRect(ulr.x, ulr.y, lrr.x, lrr.y)
   }
 
-  def draw(box: BoundingBox, colorF : CoordsPoint => Option[String])
-	  (implicit toR: ToRaster[CoordsPoint], toC: ToCoords[RasterPoint]) = {
+  def init() {
+    background(Box.stdBox)
+  }
 
-    val ul = toR(box.upperLeft)
-    val lr = toR(box.lowerRight)
+  def draw(anim: Anim): Box = {
+
+    val s = anim.paintF()
+    val box = Box.boundingBox(s.shape.boundingPoly)
+
+    val ul = raster.fromPoint(box.upperLeft)
+    val lr = raster.fromPoint(box.lowerRight)
 
     for(i <- ul.x until (lr.x+1)) {
       for(j <- ul.y until (lr.y+1)) {
-	colorF(toC(RasterPoint(i,j))) match {
-	  case Some(color) => {
-	    ctx.fillStyle = color
-	    ctx.fillRect(i,j,1,1)
-	  }
-	  case None =>
+	val p = raster.toPoint(RasterPoint(i,j))
+	if(s.shape.inside(p)) {
+	  ctx.fillStyle = s.color(p)
+	  ctx.fillRect(i,j,1,1)
 	}
       }
     }
+
+    box
   }
 
   def redraw() {
+
+    def clearBox(box: Box) {
+      background(box)
+    }
+
+    for((_, (oldbox,_)) <- objects) {
+      oldbox match {
+	case Some(box) => clearBox(box)
+	case None =>
+      }
+    }
+
+    objects = objects map {
+      case (id, (box, anim)) => id -> (Some(draw(anim)), anim)
+    }
   }
 }
