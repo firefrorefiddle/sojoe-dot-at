cc6dd311ebc64f450b0031a01c6818e0bcfd785c
Michael Hartl <mikehartl17@gmail.com>
2016-01-15 19:11:27 +0100

    serve page in available language

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -17,13 +17,25 @@ class Application extends Controller {
   def index() = page("index")
 
   def page(name: String) = Action {
+    implicit request => {
 
-    try {
-      val src = Source.fromFile(Play.getFile(s"conf/markdown/$name.md"))
-      Ok(views.html.main(name, name)(Html(pdProc.markdownToHtml(src.mkString))))
-    } catch {
-      case ex: java.io.FileNotFoundException => {
-	NotFound
+      val langs = request.acceptLanguages.map(_.code) ++ List("en", "de");
+
+      def tryLang(lang: String): Option[Source] = {
+        try {
+          val src = Source.fromFile(Play.getFile(s"conf/markdown/$lang/$name.md"))
+          Some(src)
+        } catch {
+          case ex: java.io.FileNotFoundException => {
+            None
+          }
+        }
+      }
+
+      langs flatMap tryLang headOption match {
+        case None => NotFound("Not found")
+        case Some(src) =>
+          Ok(views.html.main(name, name)(Html(pdProc.markdownToHtml(src.mkString))))
       }
     }
   }
---
+++ server/conf/markdown/de/contact.md
@@ -0,0 +1,12 @@
+Verantwortlich für den Inhalt
+=============================
+
+Michael Hartl
+
+Thor 10
+
+5760 Saalfelden
+
+Österreich
+
+<mike@sojoe.at>
